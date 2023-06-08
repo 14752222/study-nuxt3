014b4047bb1b13999bd03c77a6d7f4620a043ba1
13649 <1364952827@qq.com>
2023-06-08 23:55:52 +0800

    tailwind 在nuxtjs中使用

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+ <div>
+     <nav class="w-[50px] border-[1px] ">导航栏</nav>
+     <slot></slot>
+ </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+<h1>
+    index page
+</h1>
+
+    <NuxtLink to="/detail">detail</NuxtLink>
+</template>
+
